51ae17a5c2636abaa4b6c9d675a00366fc5eb25b
Jeffery Forbes <jefferyforbes.1@outlook.com>
2020-11-04 14:22:24 +0000

    add a dump.js to hold conflicting code

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 // Get form to take values from the user and check them against input validation requirements as per task planner documentation.
 
 // Create a TaskManager class with the following properties - taskArray = [],  methods - getAllTasks(), getTasksWithStatus(), addtask(), deleteTask(), updateTask(), assignTask()
+let taskName;
+let taskDescription;
+let taskAssignedTo;
+let taskDueDate;
+let taskStatus;
 
-// function getUserInput(newTask) {
-//     let taskName = document.querySelector(".userTaskName").value;
-//     let description = document.querySelector("#userDescription").value;
-//     let assignedTo = document.querySelector("#userAssignedTo").value;
-//     let dueDate = document.querySelector("#userDueDate").value;
-//     let status = document.querySelector("#userStatus").value;
-//     console.log(newTask)
-//     return taskName, description, assignedTo, dueDate, status;
-// };
-
-let taskName, description, assignedTo, dueDate, status;
+let taskArray = [];
 
 // The get user input functions is meant to grab the values of the user inputs from the html
 // *** At the moment I am not sure if this code works ****
 
+
 document.querySelector("#taskSubmit").addEventListener('click', function() {
-    let taskSubmitBtn = document.querySelector("#taskSubmit");
     const position = "beforeend"
+
+    let taskName = document.querySelector("#userTaskName").value;
+    let taskDescription = document.querySelector("#userDescription").value;
+    let taskAssignedTo = document.querySelector("#userAssignedTo").value;
+    let taskDueDate = document.querySelector("#userDueDate").value;
+    let taskStatus = document.querySelector("#userStatus").value;
 
     const newCard = `<div class="card">
     <div class="card-header">
         <h5>TASK</h5>
     </div>
-    <ul class="list-group list-group-flush card-space">
-        <li class="list-group-item"><span class="card-ref">Name: </span>${taskName}</li>
-        <li class="list-group-item"><span class="card-ref">Description: </span>${description}</li>
-        <li class="list-group-item"><span class="card-ref">Assign To: </span>${assignedTo}</li>
-        <li class="list-group-item"><span class="card-ref">Due Date: </span>${dueDate}</li>
-        <li class="list-group-item"><span class="card-ref">Status: </span>${status}</li>
-    </ul>`
+        <ul class="list-group list-group-flush card-space">
+            <li class="list-group-item"><span class="card-ref">Name: </span>${taskName}</li>
+            <li class="list-group-item"><span class="card-ref">Description: </span>${taskDescription}</li>
+            <li class="list-group-item"><span class="card-ref">Assign To: </span>${taskAssignedTo}</li>
+            <li class="list-group-item"><span class="card-ref">Due Date: </span>${taskDueDate}</li>
+            <li class="list-group-item"><span class="card-ref">Status: </span>${taskStatus}</li>
+            </ul>
+    </div>`
     let taskBoard = document.querySelector(".Taskboard-List")
-    if (checkAllValid == true) {
-        taskBoard.insertAdjacentHTML(position, newCard)
-    }
 
+    let allChecksPassed = validateInput(taskName, taskAssignedTo, taskDescription, taskStatus);
+    if (allChecksPassed == true) {
+        createNewTask(taskName, taskDescription, taskAssignedTo, taskStatus, taskStatus, taskArray);
+        taskBoard.insertAdjacentHTML(position, newCard)
+    } else {
+        console.log("input error")
+    };
 });
 
 // This is the event listener to clear all cards from the taskboard
@@ -51,39 +57,32 @@ document.querySelector("#taskSubmit").addEventListener('click', function() {
 });
 
 
-// the check name assign function is to check that both the name and the assign
-// values meet the validation criteria of no more than 8 characters and not empty.
+function validateInput(taskName, taskAssignedTo, taskDescription, taskStatus) {
+    let isAllValid = false;
 
-function checkAllValid() {
+    if ((taskName.length >= 3 && taskAssignedTo.length >= 3 && taskDescription.length >= 10 && taskDueDate) && taskStatus != "") {
+        isAllValid = true;
+    }
+    return isAllValid;
+}
 
-    let taskName = document.querySelector("#userTaskName").value;
-    let description = document.querySelector("#userDescription").value;
-    let assignedTo = document.querySelector("#userAssignedTo").value;
-    let dueDate = document.querySelector("#userDueDate").value;
-    let status = document.querySelector("#userStatus").value;
 
-    let checkTaskName = false;
-    let checkDescription = false;
-    let checkAssignedTo = false;
+function createNewTask(taskName, taskDescription, taskAssignedTo, taskStatus, taskStatus, taskArray) {
+    taskArray.push({
+        "name": taskName,
+        "assignedTo": taskAssignedTo,
+        "Description": taskDescription,
+        "DueDate": taskDueDate,
+        "Status": taskStatus,
+        "ID": `${taskArray.length<1 ? 1 : taskArray+1}`
+    })
+    return taskArray
+}
 
-    if (taskName.length > 0 && taskName.length < 8) {
-        checkTaskName = true;
-    } else checkTaskName = console.log("task name false");
 
-    if (description.length > 0 && description.length < 20) {
-        checkDescription = true;
-    } else checkDescription = console.log("description false");
 
-    if ((assignedTo.length > 0 && assignedTo.length < 8)) {
-        checkAssignedTo = true;
-    } else checkAssignedTo = console.log("assign false");
 
-    // (checkTaskName && checkDescription && checkAssignedTo
 
-    if (checkTaskName && checkDescription && checkAssignedTo == true) {
-        checkAllValid = true;
-    } else alert('Error! The validation checks failed! Check Inputs!')
-};
 
 // The task manager class is used as a source control or middle man iin-between
 // the user inputs and the data output onto the card list.
@@ -96,38 +95,42 @@ class TaskManager {
         this.dueDate = dueDate;
         this.status = status;
         this.taskArray = [];
-        // this.taskID = this.taskArray
+        // this.taskID = this.taskArray  
     }
 
     addTask() {
         // ---------- THIS METHOD HAS BE TEST AND WORKS ----------
-        // document.querySelector(".taskSubmit").addEventListener('click', function() {
-        //     let taskSubmitBtn = document.querySelector("#taskSubmit");
+        // document.querySelector("#taskSubmit").addEventListener('click', function() {
         //     const position = "beforeend"
 
-        //     let taskName = document.querySelector("#userTaskName").value;
-        //     let description = document.querySelector("#userDescription").value;
-        //     let assignedTo = document.querySelector("#userAssignedTo").value;
-        //     let dueDate = document.querySelector("#userDueDate").value;
-        //     let status = document.querySelector("#userStatus").value;
+        //     taskName = document.querySelector("#userTaskName").value;
+        //     taskDescription = document.querySelector("#userDescription").value;
+        //     taskAssignedTo = document.querySelector("#userAssignedTo").value;
+        //     taskDueDate = document.querySelector("#userDueDate").value;
+        //     taskStatus = document.querySelector("#userStatus").value;
 
         //     const newCard = `<div class="card">
         //     <div class="card-header">
         //         <h5>TASK</h5>
         //     </div>
-        //     <ul class="list-group list-group-flush card-space">
-        //         <li class="list-group-item"><span class="card-ref">Name: </span>${taskName}</li>
-        //         <li class="list-group-item"><span class="card-ref">Description: </span>${description}</li>
-        //         <li class="list-group-item"><span class="card-ref">Assign To: </span>${assignedTo}</li>
-        //         <li class="list-group-item"><span class="card-ref">Due Date: </span>${dueDate}</li>
-        //         <li class="list-group-item"><span class="card-ref">Status: </span>${status}</li>
-        //     </ul>`
-
+        //         <ul class="list-group list-group-flush card-space">
+        //             <li class="list-group-item"><span class="card-ref">Name: </span>${taskName}</li>
+        //             <li class="list-group-item"><span class="card-ref">Description: </span>${taskDescription}</li>
+        //             <li class="list-group-item"><span class="card-ref">Assign To: </span>${taskAssignedTo}</li>
+        //             <li class="list-group-item"><span class="card-ref">Due Date: </span>${taskDueDate}</li>
+        //             <li class="list-group-item"><span class="card-ref">Status: </span>${taskStatus}</li>
+        //             </ul>
+        //     </div>`
         //     let taskBoard = document.querySelector(".Taskboard-List")
 
-        //     taskBoard.insertAdjacentHTML(position, newCard)
-    };
-    clearAllTasks() {
+        //     let allChecksPassed = validateInput(taskName, taskAssignedTo, taskDescription, taskStatus);
+        //     if (allChecksPassed == true) {
+        //         taskBoard.insertAdjacentHTML(position, newCard)
+        //     } else {
+        //         console.log("input error")
+        //     };
+        // });        
+        // clearAllTasks() {
         // -------------- THIS METHOD HAS BE TEST AND WORKS ----------------------
         // document.querySelector("#taskSubmit").addEventListener('click', function() {
         //     const clear = document.querySelector("#clear-cards")
@@ -138,3 +141,5 @@ class TaskManager {
         // });
     };
 }
+
+// let TaskManager = new TaskManager(taskArray);
---
+++ dump.js
@@ -0,0 +1,32 @@
+// if (allcheckAllValid === true) {
+//     taskBoard.insertAdjacentHTML(position, newCard);
+//     console.log("this should insert a task")
+// }
+// });
+
+
+
+
+// function checkAllValid(taskName, description, assignedTo, dueDate, status) {
+// let checkTaskName = false;
+// let checkDescription = false;
+// let checkAssignedTo = false;
+// let allcheckAllValid = false;
+
+//     if (taskName.length > 3) {
+//         checkTaskName = true;
+//     } else { console.log("taskname false") }
+
+//     if (description.length > 10) {
+//         checkDescription = true;
+//     } else { console.log("description false") }
+
+//     if (assignedTo.length > 3) {
+//         checkAssignedTo = true;
+//     } else { console.log("assign false") }
+
+//     if (checkTaskName && checkDescription && checkAssignedTo === true) {
+//         allcheckAllValid = true;
+//     } else { console.log(`Error! The validation checks failed! task name is ${checkTaskName}, description is ${checkDescription} and assigned to is ${checkAssignedTo}`) }
+//     return allcheckAllValid;
+// };
